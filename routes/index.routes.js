a990370cd8bb0daa300f35c6895ada67f993f5a0
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-09-09 22:25:02 -0500

    newSpotPricesRoutes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,32 +3,19 @@ const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 const cryptoInvRoutes = require("./cryptoInv.routes");
 const financesRoutes = require("./finances.routes");
+const cryptoPriceRoutes = require("./cryptoPrice.routes");
 
-const binance = require("../config/binance.config");
 
 
-router.get("/", (req, res, next) => {
-  
-  binance.websockets.candlesticks(['ETHUSDT'], "1m", (candlesticks) => {
-    let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks;
-    let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
-    console.info(symbol+" "+interval+" candlestick update");
-    console.info("open: "+open);
-    console.info("high: "+high);
-    console.info("low: "+low);
-    console.info("close: "+close);
-    console.info("volume: "+volume);
-    console.info("isFinal: "+isFinal);
+router.get('/',(req, res) => {
+  res.status(200).json("all good here")
+})
 
-    res.json({closingPrice:close})
-  }); 
-
-res.json({successMessage:"all great"})
-});
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/admin", cryptoInvRoutes);
 router.use("/admin", financesRoutes);
+router.use("/cryptoprice",cryptoPriceRoutes)
 
 module.exports = router;
