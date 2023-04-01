93efd0d2afae94ee5d75143767e07419faf70c79
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-04-01 14:42:31 -0500

    implement websocket connection for binance api

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,17 +5,14 @@ const cryptoInvRoutes = require("./cryptoInv.routes");
 const financesRoutes = require("./finances.routes");
 const cryptoPriceRoutes = require("./cryptoPrice.routes");
 
-
-
-router.get('/',(req, res) => {
-  res.status(200).json("all good here")
-})
-
+router.get("/", (req, res) => {
+  res.status(200).json("all good here");
+});
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/admin", cryptoInvRoutes);
 router.use("/admin", financesRoutes);
-router.use("/cryptoprice",cryptoPriceRoutes)
+router.use("/cryptoprice", cryptoPriceRoutes);
 
 module.exports = router;
